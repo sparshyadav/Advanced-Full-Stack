ec18316992686b5534803e716a871b051435adda
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 17:50:58 +0530

    Starting with TypeScript

---
+++ TypeScript/a.js
@@ -0,0 +1,7 @@
+"use strict";
+let buddhu_bacchi = "Sweta Shreya Thakurayan";
+let number = -1;
+let bol = true;
+console.log(number);
+console.log(bol);
+console.log(buddhu_bacchi);
---
+++ TypeScript/a.ts
@@ -0,0 +1,8 @@
+let buddhu_bacchi: string = "Sweta Shreya Thakurayan";
+let number: number = -1;
+let bol: boolean = true;
+
+
+console.log(number);
+console.log(bol);
+console.log(buddhu_bacchi);
