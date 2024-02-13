5b87e346c41485756160648c4ba58b39c68ffbf9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 17:26:56 +0530

    JWT Login Completed

--- Login/auth/auth.js
+++ Login/auth/auth.js
@@ -1,6 +1,19 @@
 const jwt=require("jsonwebtoken");
-const secretKey="naag skati";
+const secretKey="shhhh";
 
 module.exports.createWebToken=(user)=>{
     return jwt.sign(user, secretKey, {expiresIn: "24h"});
+}
+
+module.exports.verifyToken=(req, res)=>{
+    let token=req.cookies.token;
+    let decode= jwt.verify(token, secretKey);
+
+    console.log(decode);
+
+    if(decode){
+        return next();
+    }
+
+    res.send("Token not Valid");
 }
--- Login/server.js
+++ Login/server.js
@@ -1,8 +1,9 @@
 const express=require("express");
+const cookieParser=require("cookie-parser");
 const User=require("./models/user");
 const mongoose=require("mongoose");
 const bcrypt=require("bcrypt");
-const {createWebToken}=require("./auth/auth");
+const {createWebToken, verifyToken}=require("./auth/auth");
 const saltRounds=10;
 const app=express();
 const path=require("path");
@@ -11,18 +12,16 @@ app.use(express.static(path.join(__dirname, "static")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.set("view engine", 'hbs');
+app.use(cookieParser());
 
-app.get("/", (req, res)=>{
+app.set('view engine', 'hbs');
+
+app.get("/", verifyToken, (req, res)=>{
     res.render(("home"));
 });
 
 app.get("/signup", (req, res)=>{
     res.render(signup);
-})
-
-app.get("/login", (req, res)=>{
-    res.render(login);
 })
 
 app.post("/signup", async (req, res)=>{
@@ -36,6 +35,10 @@ app.post("/signup", async (req, res)=>{
     res.json(newUser);
 });
 
+app.get("/login", (req, res)=>{
+    res.render(login);
+})
+
 app.post("/login", async(req, res)=>{
     const {email, password}=req.body;
 
@@ -46,6 +49,7 @@ app.post("/login", async(req, res)=>{
     await bcrypt.compare(password, user.password).then(function(result){
         if(result){
             let token=createWebToken(user.toJSON());
+            res.cookie("token", token);
             res.json(token);
         }
     })
