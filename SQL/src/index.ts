66b2bb24b225ca1a4dde4da3e0da92e8f759b037
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-20 17:36:06 +0530

    Creating Schema

--- SQL/src/index.ts
+++ SQL/src/index.ts
@@ -7,5 +7,17 @@ const client = new Client({
 async function connect() {
     await client.connect();
 }
-
 connect();
+
+async function createPost() {
+    const query = `CREATE TABLE posts(
+        id SERIAL PRIMARY KEY,
+        postname varchar(50) UNIQUE NOT NULL
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE CURRENT_TIMESTAMP
+    )`;
+
+    let result = await client.query(query);
+    console.log(result);
+}
