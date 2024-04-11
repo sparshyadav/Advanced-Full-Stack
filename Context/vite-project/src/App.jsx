b598093421ab4c59a5a6727af80dbcd671c6e7f4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-11 17:29:34 +0530

    Updating Context

--- Context/vite-project/src/App.jsx
+++ Context/vite-project/src/App.jsx
@@ -2,47 +2,96 @@ import { useState, useContext } from 'react'
 import { createContext } from 'react';
 import './App.css'
 
-const myContext = createContext(0);
+// const myContext = createContext(0);
+const sonaKahaHai = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [sonaKahaHai, setSonaKahaHai]=useState(null);
 
   return (
     <>
-      <h1>I am the APP Component</h1>
-      <myContext.Provider value={count}>
-        <FirstChild></FirstChild>
-      </myContext.Provider>
+      <h1>I am the App Component </h1>
+      <Grandfather></Grandfather>
     </>
   )
 }
 
-function FirstChild() {
+function Grandfather() {
   return (
     <>
-      <h1>I am inside App</h1>
-      <SecondChild></SecondChild>
+      <h1>I am GrandFather</h1>
+      <sonaKahaHai.Provider value="Bade W ke Neeche">
+        <Parent></Parent>
+      </sonaKahaHai.Provider>
     </>
   )
 }
 
-function SecondChild() {
+function Parent() {
   return (
     <>
-      <h1>I am inside First Child</h1>
-      <Thirdhild></Thirdhild>
+      <h1>I am Parent</h1>
+      <Child></Child>
     </>
   )
 }
 
-function Thirdhild() {
-  const value = useContext(myContext)
+function Child() {
   return (
     <>
-      <h1>I am inside Second Child {value}</h1>
+      <h1>I am Child</h1>
+      <Consumer></Consumer>
     </>
   )
 }
+
+function Consumer() {
+  const value = useContext(sonaKahaHai);
+  return (
+    <>
+      <h1>I am Consumer {value}</h1>
+    </>
+  )
+}
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <h1>I am the APP Component</h1>
+//       <myContext.Provider value={count}>
+//         <FirstChild></FirstChild>
+//       </myContext.Provider>
+//     </>
+//   )
+// }
+
+// function FirstChild() {
+//   return (
+//     <>
+//       <h1>I am inside App</h1>
+//       <SecondChild></SecondChild>
+//     </>
+//   )
+// }
+
+// function SecondChild() {
+//   return (
+//     <>
+//       <h1>I am inside First Child</h1>
+//       <Thirdhild></Thirdhild>
+//     </>
+//   )
+// }
+
+// function Thirdhild() {
+//   const value = useContext(myContext)
+//   return (
+//     <>
+//       <h1>I am inside Second Child {value}</h1>
+//     </>
+//   )
+// }
 
 export default App
-  `1q21 `
